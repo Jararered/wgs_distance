bf37f6ec3687cbf33839fd1b06ada29514772ed0
Jararered <Jararered@icloud.com>
2021-02-08 18:24:28 -0500

    oops

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='wgs_distance',
     version='0.1',
-    packages=[''],
+    packages=[],
     url='https://github.com/Jararered/wgs_distance',
     license='N/A',
     author='Jared Nichols',
