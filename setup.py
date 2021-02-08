92df6ed8189d5b044dba66cc02e94379907879e3
Jararered <Jararered@icloud.com>
2021-02-08 18:13:35 -0500

    packages

--- setup.py
+++ setup.py
@@ -3,13 +3,10 @@ from setuptools import setup
 setup(
     name='wgs_distance',
     version='0.1',
-    packages=[''],
+    packages=['PyQt5'],
     url='https://github.com/Jararered/wgs_distance',
     license='N/A',
     author='Jared Nichols',
     author_email='Jararered@icloud.com',
     description='WGS-84 Distance Calculator',
-    install_requires=[
-        'PyQt5',
-    ]
 )
